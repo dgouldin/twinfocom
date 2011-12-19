0e3a4d15c73dfe1be806a48a756ed2293e18f7d9
David Gouldin <david@gould.in>
2011-12-18 23:12:38 -0800

    Adding basic subprocess support and output formatting. It begins.

---
+++ dumb_frotz.py
@@ -0,0 +1,58 @@
+import os
+import subprocess
+
+DFROTZ = '/usr/local/bin/dfrotz'
+
+def format_output(output, load, command, save):
+    """
+    Yeah, it's ugly.
+    Big whoop.
+    Wanna fight about it?
+    """
+    lines = filter(lambda l: l and l != '>', output.split('\n'))
+    from_line = 5
+    to_line = 0
+    if command or load:
+        from_line += 4
+    if load:
+        from_line += 2
+    if save:
+        to_line = -1
+    lines = lines[from_line:]
+    if to_line:
+        lines = lines[:to_line]
+    where_line = lines.pop(0).replace('>', '').strip()
+    where = where_line.split('    ', 1)[0]
+    return '[%s] %s' % (where, ' '.join(lines))
+
+def execute(game_path, command=None, save_path=None):
+    args = [DFROTZ, game_path]
+    proc = subprocess.Popen(args, stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+    load = save_path and os.path.exists(save_path)
+    save = save_path is not None
+
+    lines = []
+    if load:
+        # load previously saved game
+        lines.extend([
+            'restore',
+            save_path,
+        ])
+    if command:
+        lines.append(command)
+    if save:
+        lines.extend([
+            'save',
+            save_path,
+        ])
+        if load:
+            # confirm overwrite
+            lines.append('y')
+
+    if lines:
+        output, _ = proc.communicate(input='\n'.join(lines) + '\n')
+    else:
+        output, _ = proc.communicate()
+
+    return format_output(output, load, command is not None, save)
+
